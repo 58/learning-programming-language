054e2337c96a19a232b29f6192d1e5b9c50a91e8
Pear <housenka@outlook.com>
2024-05-21 21:58:00 +0900

    Add a feature that displays the number of guesses made

--- rust/hit_num_game/src/main.rs
+++ rust/hit_num_game/src/main.rs
@@ -1,11 +1,38 @@
+use std::fs::File;
 use std::io;
+use std::io::prelude::*;
+use std::path::Path;
 use rand::Rng;
 
 fn main() {
+    // display the number of times you have guessed
+    const FILE_NAME: &str = "hit_num_game.log";
+    let path = Path::new(FILE_NAME);
+    let display = path.display();
+    let mut answer_times = 0;
+
+    if path.exists() {
+        let mut file = match File::open(&path) {
+            Err(why) => panic!("couldn't open {}: {}", display, why),
+            Ok(file) => file,
+        };
+
+        let mut answered_times = String::new();
+        match file.read_to_string(&mut answered_times) {
+            Err(why) => panic!("couldn't read {}: {}", display, why),
+            Ok(_) => (),
+        }
+
+        println!("You have guessed {} times.", answered_times);
+    } else {
+        match File::create(&path) {
+            Err(why) => panic!("couldn't create {}: {}", display, why),
+            Ok(_) => (),
+        };
+    }
+
     // generate random number
     let answer = rand::thread_rng().gen_range(1..101); 
-
-    let mut answer_times = 0;
 
     loop {
         answer_times += 1;
@@ -15,6 +42,13 @@ fn main() {
         let mut buffer = String::new();
         io::stdin().read_line(&mut buffer).expect("Failed to read line.");
         let input_num = buffer.trim().parse::<i32>().unwrap();
+
+        // game over
+        if answer_times == 10 {
+            println!("You lose!");
+            println!("The answer is {}.", answer);
+            break;
+        }
 
         // evaluate your answer
         if input_num == answer {
@@ -30,12 +64,15 @@ fn main() {
             println!("Too big!");
             continue;
         }
+    }
 
-        // game over
-        if answer_times == 10 {
-            println!("You lose!");
-            println!("The answer is {}.", answer);
-            break;
-        }
+    // write the number of times you have guessed
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}", display, why),
+        Ok(file) => file,
+    };
+    match file.write_all(answer_times.to_string().as_bytes()) {
+        Err(why) => panic!("couldn't write to {}: {}", display, why),
+        Ok(_) => (),
     }
 }
